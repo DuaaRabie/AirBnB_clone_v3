52c3fa510923b18f2e68a7d7c4729ba0ec784417
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-16 22:44:57 +0000

    task2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,13 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ A method to retrieve one object """
+        if not id or not cls:
+            return None
+        return cls.__session.query.get(id)
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage"""
+        return self.__session.query(cls).count()
